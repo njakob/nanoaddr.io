918abeb306b835f875ae5fac7ccf57d750f5e1d4
Nicolas Jakob <nicow.jakob@gmail.com>
2018-03-06 08:51:27 +0100

    chore: Update disclaimer content

--- src/nanoaddr/components/Disclaimer.jsx
+++ src/nanoaddr/components/Disclaimer.jsx
@@ -6,6 +6,7 @@ import FakeLink from 'nanoaddr/components/FakeLink';
 
 const Container = styled.div`
   padding: 16px 0;
+  max-width: 800px;
   color: ${props => props.theme.colors.g1};
   user-select: none;
 `
@@ -41,7 +42,7 @@ class Disclaimer extends React.Component<{}, State> {
         </Header>
         {this.state.shown && (
           <Content>
-            Disclaimer: Nanoaddr.io takes no responsibility for the use of generated content provided by this website.
+            The addresses and private keys are generated directly in your bowser without the involvment of any servers and are not transmitted over the Internet. For additional security we still recommend that you disconnect your computer from the Internet while using this site.
           </Content>
         )}
       </Container>
