130be7045dd58f11c4fb799a2e1ac8dc7dae2e08
Nicolas Jakob <nicow.jakob@gmail.com>
2018-03-19 17:17:41 +0100

    chore: Improve FAQ

--- src/nanoaddr/components/Posts.jsx
+++ src/nanoaddr/components/Posts.jsx
@@ -4,21 +4,28 @@ import * as React from 'react';
 import styled, { css } from 'styled-components';
 import Link from 'nanoaddr/components/Link';
 
+const URL_NANO = 'https://nano.org';
+const URL_NANO_WALLET = 'https://nanowallet.io';
+const URL_NANO_VAULT = 'http://www.nanovault.io';
+const URL_NANO_MINER = 'https://nano-miner.com/?908l';
+const URL_NANO_FAUCET = 'https://www.nanofaucet.org';
+const URL_BINANCE = 'https://www.binance.com/?ref=20233638';
+
 const articles = [
   {
     title: 'What is Nano?',
     content: (
-      <p><Link href="https://nano.org">Nano</Link> is a cryptocurrency that provides in instant, feeless transactions. Nano has been previously know as RaiBlocks.</p>
+      <p><Link href={URL_NANO}>Nano</Link> is a cryptocurrency that provides in instant, feeless transactions. Nano has been previously know as RaiBlocks.</p>
     ),
   },
   {
     title: 'Why Nano?',
     content: (
-      <p>Nano has no transaction fees. This particularity makes it perfect for small payouts</p>
+      <p>Nano has no transaction fees. This particularity makes it perfect for small payouts.</p>
     ),
   },
   {
-    title: 'Is NanoAddr.io safe?',
+    title: 'Is NanoAddr safe?',
     content: (
       <p>Nothing on the Internet is totally safe. We strongly recommend to run the search while being offline and in private mode.</p>
     ),
@@ -27,6 +34,21 @@ const articles = [
     title: 'How the seed is randomly generated?',
     content: (
       <p>We use a generator provided by the browser directly which creates cryptographically strong random values. <code><Link href="https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues" rel="nofollow">Crypto.getRandomValues</Link></code></p>
+    ),
+  },
+  {
+    title: 'How can I get some Nano?',
+    content: (
+      <React.Fragment>
+        <p><Link href={URL_BINANCE}>Binance.com</Link> is one exchange where you would be able to buy Nano.</p>
+        <p>If you want just to play around, you can use <Link href={URL_NANO_MINER}>nano-miner.com</Link> or <Link href={URL_NANO_FAUCET}>nanofaucet.org</Link>.</p>
+      </React.Fragment>
+    ),
+  },
+  {
+    title: 'Where can I get a wallet?',
+    content: (
+      <p>You can use <Link href={URL_NANO_WALLET}>nanowallet.io</Link> or <Link href={URL_NANO_VAULT}>nanovault.io</Link> as wallets and import the seeds you are finding on NanoAddr.</p>
     ),
   },
 ];
